0e4e5f6d5f0666fa8c08c34fe2d97155ffef1be4
daitomiun <mateo.rodriguez.fierro@gmail.com>
2026-02-07 18:01:42 -0500

    Effect.ts and pdfjs test implemented
    Svelte and backend scaffolding created

---
+++ frontend/src/lib/components/pdf-cavnas.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import loadPDFWithEffect from "$lib/pdfjs";
+</script>
+
+<canvas use:loadPDFWithEffect={"../../static/sample-local-pdf.pdf"}></canvas>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import "../app.css";
+	import PdfCavnas from "$lib/components/pdf-cavnas.svelte";
+</script>
+
+<PdfCavnas></PdfCavnas>
